9b4d7f9439708cb32f5f6ff5a7f51bb6f4ab45aa
darwin the third <darwing8896@gmail.com>
2023-08-14 11:16:38 -0400

    DID AI

--- src/routes/evaluate/+page.svelte
+++ src/routes/evaluate/+page.svelte
@@ -1,21 +1,31 @@
 <script>
 
 </script>
-<main>
+<main class="text-center bg-emerald-600 font-mono">
 
-<div>
-    <h3>August 13, 2023: 10:15 am</h3>
-    <p>
-        The journal entry reflects feelings of depression and low self-esteem. The individual feels inadequate and isolated, as if they don't fit in anywhere. They're struggling with negative thoughts about themselves and are overly concerned about how they might come across to others. This could suggest symptoms of depression and social anxiety.
-    </p>
-    <h3>August 15, 2023: 4:30 pm</h3>
-    <p>The individual seems to have taken a positive step by making new friends and stepping out of their comfort zone. This could indicate a desire for social connection and an effort to overcome their feelings of isolation. However, it's important to note that even though they're trying not to care about others' opinions, their struggles from the previous entry might still be present to some extent.
+<h1 class="font-bold  p-8 text-2xl  text-red-50">What AI Evaluation will look like after we buy all FAANG companies</h1>
 
-        It's important to remember that online interpretations can only provide general insights and should not be used as a substitute for professional advice. If you or someone you know is struggling with mental health concerns, it's recommended to consult a licensed mental health professional who can provide personalized guidance and support.
-    </p>
+<div class="">
+    <div class="bg-emerald-900 m-5 p-5 text-red-50 rounded-lg shadow-2xl">
+        <h3 class="font-bold">August 13, 2023: 10:15 am</h3>
+        <p>
+            The journal entry reflects feelings of depression and low self-esteem. The individual feels inadequate and isolated, as if they don't fit in anywhere. They're struggling with negative thoughts about themselves and are overly concerned about how they might come across to others. This could suggest symptoms of depression and social anxiety.
+        </p>
+    </div>
+    <div class="bg-emerald-900 m-5 p-5 text-red-50 rounded-lg shadow-2xl">
+        <h3 class="font-bold" >August 15, 2023: 4:30 pm</h3>
+        <p>The individual seems to have taken a positive step by making new friends and stepping out of their comfort zone. This could indicate a desire for social connection and an effort to overcome their feelings of isolation. However, it's important to note that even though they're trying not to care about others' opinions, their struggles from the previous entry might still be present to some extent.
+
+            It's important to remember that online interpretations can only provide general insights and should not be used as a substitute for professional advice. If you or someone you know is struggling with mental health concerns, it's recommended to consult a licensed mental health professional who can provide personalized guidance and support.
+        </p>
+    </div>
 </div>
 
 </main>
 <style>
-
+    main {
+        height: 50cm;
+        background: rgb(177,219,207);
+        background: radial-gradient(circle, rgba(177,219,207,1) 0%, rgba(32,208,126,1) 100%); 
+    }
 </style>
