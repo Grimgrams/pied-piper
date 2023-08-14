9caf577d8bef46b114d10d2dbc0aba40f3055c06
Julian <julianjpit@gmail.com>
2023-08-14 10:36:25 -0400

    trying to fix merge error

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,3 @@
 // place files you want to import through the `$lib` alias in this folder.
+export const journaled = '/journal'
+export const aipagee = '/src/lib/routes/aipage'
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -26,13 +26,26 @@
             {/each}
     </div>
 
-    <form action="?/addEntry" method="POST">
-        <h3>New Entry</h3>
-        <input class=""  name="topic" placeholder="Topic..." autocomplete="off" />
-        <input class=""  name="body" placeholder="What's on your mind?" autocomplete="off"/>
-        <button type="submit">Add Entry</button>
+    <div class="flexy">
+        <!--  -->
+        <div  class="w-4/6 rounded-xl p-3 bg-#0d9488 shadow-2xl m-2 space-x-6">
 
-    </form>
+            <form action="?/addEntry" method="POST" class="entry">
+                <label class="text-stone-100 bg-emerald-600 font-bold font-mono rounded-xl p-3 mt-5"for="entry">New Entry: </label><br>
+                <textarea class="h-11 rounded-xl p-3 my-2 mt-5"  name="topic" placeholder="Title" autocomplete="off"></textarea><br>
+                <textarea class="h-96 rounded-xl p-3 " rows="4" cols="50" name="body" placeholder="Type your fitness journal entry here..." autocomplete="off"></textarea>
+
+
+                <div class="text-green-700 bg-emerald-300 m-2 rounded-xl font-bold font-mono p-4 shadow-2xl " ><input  type="submit" value="Submit"></div>
+
+
+            </form>
+
+        </div>
+
+        <div class="w-4/6 rounded-xl p-3 bg-#0d9488 shadow-2xl m-2 space-x-6">
+
+            <div class="">
     <a href="/progress">Progress</a>
 </div>
 
