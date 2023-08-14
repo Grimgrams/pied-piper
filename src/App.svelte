e8fe6fe797ba965668491e9d28c4a89ee4862526
darwin the third <darwing8896@gmail.com>
2023-08-14 03:37:21 -0400

    added more ui shit

--- src/App.svelte
+++ src/App.svelte
@@ -8,29 +8,16 @@
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
 
-  <div class="card">
-    <Counter />
-  </div>
+  <form class="entry">
+    <label class="entrylabel"for="entry">New Entry: </label><br>
+    <textarea class="maintype" rows="4" cols="50" ></textarea>
+  </form>
 
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
 
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
 </main>
 
+<!-- 
 <style>
   .logo {
     height: 6em;
@@ -48,3 +35,4 @@
     color: #888;
   }
 </style>
+-->
